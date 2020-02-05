24ee5cd49d992ef1af6b9caa7ce3ed2ecd301e74
Bruno <b-coimbra@hotmail.com>
2020-02-05 18:40:41 -0300

    recursive sum

--- src/parser.rs
+++ src/parser.rs
@@ -9,16 +9,15 @@ fn next_tokens(tokens: &lexer::TokenList, pos: usize) -> lexer::TokenList {
         .collect()
 }
 
-// TODO: Create a container for all operations 
-// Operation::sum(), Operation::div() etc
 fn sum_operation(tokens: &lexer::TokenList, mut sum: u32) -> u32 {
     for token in tokens {
         match token.kind {
-            lexer::TokenKind::Symbol(lexer::Symbol::RParen) => return sum,
+            lexer::TokenKind::Symbol(lexer::Symbol::LParen) |
+            lexer::TokenKind::Operator(lexer::Operator::Plus) => {
+                return sum_operation(&next_tokens(tokens, token.position), sum);
+            },
             lexer::TokenKind::Type(lexer::Type::Int) => {
                 sum += ext::to_u32(&token.value);
-
-                return sum_operation(&next_tokens(tokens, token.position), sum);
             },
             _ => ()
         }
@@ -30,16 +29,18 @@ pub fn parse(tokens: &lexer::TokenList, mut opened_parens: bool) -> () {
     if tokens.is_empty() { return }
 
     for token in tokens {
+        let next_tokens: lexer::TokenList = next_tokens(tokens, token.position);
+
         match token.kind {
             lexer::TokenKind::Symbol(lexer::Symbol::LParen) => {
                 opened_parens = true;
 
-                return parse(&next_tokens(tokens, token.position), opened_parens);
+                return parse(&next_tokens, opened_parens);
             },
             lexer::TokenKind::Operator(lexer::Operator::Plus) => {
-                // TODO: make nested expressions work by gathering the sub expressions and replacing them (evaluating)
+                // TODO: gather the sub expressions and replacing them (evaluating)
                 // to a new token with the value from the result
-                println!("{}", sum_operation(&next_tokens(tokens, token.position), 0));
+                println!("{}", sum_operation(&next_tokens, 0));
                 return ();
             },
             lexer::TokenKind::Symbol(lexer::Symbol::RParen) => {
